c6e849588482560df8885df91b751b7a5b8578b4
LobbeyTan <tancheelam2@gmail.com>
2023-01-03 22:17:24 +0800

    Update poxis commands

--- 1_🏠_Home_Page.py
+++ 1_🏠_Home_Page.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import os
+from subprocess import run
+
+if 'posix' in os.name:
+    run("pip uninstall opencv-python")
 
 st.set_page_config(layout="wide", page_title="Low Light Image Enhancement")
 

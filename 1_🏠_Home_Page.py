8e995df40c77f6453460055139da4faa589ad001
LobbeyTan <tancheelam2@gmail.com>
2023-01-03 11:45:00 +0800

    Update requirements.txt

--- 1_🏠_Home_Page.py
+++ 1_🏠_Home_Page.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import os
+from subprocess import run
+
+if os.name == 'poxis':
+    run("sudo apt-get install python3-opencv")
 
 st.set_page_config(layout="wide", page_title="Low Light Image Enhancement")
 
